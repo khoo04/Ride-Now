0d4a71f4ebf69f202c0244a97cd269ebacdc84a2
Khoo Zhen Xian <khoozhenxian@gmail.com>
2025-01-02 18:10:51 +0800

    Disable demo payment in production

--- lib/features/payment/presentation/pages/payment_web_screen.dart
+++ lib/features/payment/presentation/pages/payment_web_screen.dart
@@ -60,11 +60,10 @@ Page resource error:
           },
           onUrlChange: (UrlChange change) {
             if (change.url ==
-                "https://khoodev.us.kg/api/RideNowV1/payment/callback") {
+                "https://khoodev.blog/api/RideNowV1/payment/callback") {
               Navigator.of(context)
                   .popAndPushNamed(PaymentResultScreen.routeName);
             }
-            eLog('url change to ${change.url}');
           },
         ),
       )
@@ -84,85 +83,86 @@ Page resource error:
       backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: const Text('Ride Payment'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              bool demoPaymentResult = false;
-              demoPaymentResult = await showDialog(
-                context: context,
-                builder: (context) {
-                  final passwordController = TextEditingController();
+        //FIXME: Disable Demo Payment in Production
+        // actions: [
+        //   IconButton(
+        //     onPressed: () async {
+        //       bool demoPaymentResult = false;
+        //       demoPaymentResult = await showDialog(
+        //         context: context,
+        //         builder: (context) {
+        //           final passwordController = TextEditingController();
 
-                  return AlertDialog(
-                    title: const Text("Demo Payment"),
-                    content: StatefulBuilder(
-                      builder: (context, setState) {
-                        return Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Text("This is a demo payment process."),
-                            const SizedBox(height: 20),
-                            SwitchListTile(
-                              title: const Text('Success'),
-                              value: demoPaymentResult,
-                              onChanged: (bool value) {
-                                setState(() {
-                                  demoPaymentResult = value;
-                                });
-                              },
-                            ),
-                            TextField(
-                              controller: passwordController,
-                              decoration:
-                                  const InputDecoration(labelText: "Password"),
-                              obscureText: true,
-                            ),
-                            ElevatedButton(
-                              onPressed: () async {
-                                final networkClient =
-                                    GetIt.instance<NetworkClient>();
+        //           return AlertDialog(
+        //             title: const Text("Demo Payment"),
+        //             content: StatefulBuilder(
+        //               builder: (context, setState) {
+        //                 return Column(
+        //                   mainAxisSize: MainAxisSize.min,
+        //                   children: [
+        //                     const Text("This is a demo payment process."),
+        //                     const SizedBox(height: 20),
+        //                     SwitchListTile(
+        //                       title: const Text('Success'),
+        //                       value: demoPaymentResult,
+        //                       onChanged: (bool value) {
+        //                         setState(() {
+        //                           demoPaymentResult = value;
+        //                         });
+        //                       },
+        //                     ),
+        //                     TextField(
+        //                       controller: passwordController,
+        //                       decoration:
+        //                           const InputDecoration(labelText: "Password"),
+        //                       obscureText: true,
+        //                     ),
+        //                     ElevatedButton(
+        //                       onPressed: () async {
+        //                         final networkClient =
+        //                             GetIt.instance<NetworkClient>();
 
-                                if (passwordController.text.trim() !=
-                                    "KH00_DEMO") {
-                                  showSnackBar(context, "Invalid password");
-                                  return;
-                                }
+        //                         if (passwordController.text.trim() !=
+        //                             "KH00_DEMO") {
+        //                           showSnackBar(context, "Invalid password");
+        //                           return;
+        //                         }
 
-                                String token = url.split('/').last;
-                                final response = await networkClient.invoke(
-                                    "${ApiRoutes.apiBaseUrl}/payment/demo/$token",
-                                    RequestType.post,
-                                    requestBody: {
-                                      "password": "KH00_DEMO",
-                                      "demo_status": demoPaymentResult ? 1 : 0,
-                                    });
+        //                         String token = url.split('/').last;
+        //                         final response = await networkClient.invoke(
+        //                             "${ApiRoutes.apiBaseUrl}/payment/demo/$token",
+        //                             RequestType.post,
+        //                             requestBody: {
+        //                               "password": "KH00_DEMO",
+        //                               "demo_status": demoPaymentResult ? 1 : 0,
+        //                             });
 
-                                bool success = response.data["success"] as bool;
-                                // Call your demo payment logic here
-                                // You can use your API or local logic to trigger a demo payment
-                                if (context.mounted && success) {
-                                  Navigator.of(context)
-                                      .pop(success); // Close the dialog
-                                }
-                                // Handle demo payment logic
-                              },
-                              child: const Text("Start Demo Payment"),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  );
-                },
-              );
-              if (context.mounted && demoPaymentResult) {
-                Navigator.of(context)
-                    .popAndPushNamed(PaymentResultScreen.routeName);
-              }
-            },
-            icon: const Icon(Icons.lock_open_rounded),
-          ),
-        ],
+        //                         bool success = response.data["success"] as bool;
+        //                         // Call your demo payment logic here
+        //                         // You can use your API or local logic to trigger a demo payment
+        //                         if (context.mounted && success) {
+        //                           Navigator.of(context)
+        //                               .pop(success); // Close the dialog
+        //                         }
+        //                         // Handle demo payment logic
+        //                       },
+        //                       child: const Text("Start Demo Payment"),
+        //                     ),
+        //                   ],
+        //                 );
+        //               },
+        //             ),
+        //           );
+        //         },
+        //       );
+        //       if (context.mounted && demoPaymentResult) {
+        //         Navigator.of(context)
+        //             .popAndPushNamed(PaymentResultScreen.routeName);
+        //       }
+        //     },
+        //     icon: const Icon(Icons.lock_open_rounded),
+        //   ),
+        // ],
       ),
       body: WebViewWidget(controller: _controller),
     );
